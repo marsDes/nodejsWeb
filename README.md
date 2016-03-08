53b917deab62b196b7580170700428f031e9bb8e
林建歆 <qq@qq.com>
2016-03-08 09:15:26 +0800

    修改

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # nodejsWeb
 nodejs构建简单电影网站
+
+需要安装的模块
+npm install body-parser bootsharp express jade moment mogoose underscore
+npm install -g bower
+
+
 常见问题
 npm 安装bootsharp报错path 解决：使用Git Bash来代替cmd 命令 bower install bootstrap
--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express=require('express')
 var path=require('path')
 var bodyParser = require('body-parser')
+var mongoose= require('mongoose')
+var Movie=require('./modules/movie')
+var _=require('underscore')
 var port=process.env.POTR||3000
 var app=express()
+
+mongoose.connect('mongodb://localhost/imooc')
 
 app.set('views','./views/pages')
 app.set('view engine','jade')
@@ -15,39 +20,103 @@ app.listen(port)
 console.log("website started on prot"+port)
 
 //index page
- app.get('/',function(req, res){
- 	res.render('index',{
- 		title:'测试 首页',
- 		movies:[{title:'机械战警',
- 		_id:1,
- 		poster:'http://img.mukewang.com/566532b30001c09c06000338-240-135.jpg'
-
- 	},{
- 		title:'地狱战警',
- 		_id:2,
- 		poster:'http://img.mukewang.com/566532b30001c09c06000338-240-135.jpg'
- 	},{
- 		title:'x战警',
- 		_id:3,
- 		poster:'http://img.mukewang.com/566532b30001c09c06000338-240-135.jpg'
- 	}]
+app.get('/',function(req, res){
+ 	Movie.fetch(function(err, movies){
+ 		if (err) {
+ 			console.log(err)
+ 		}
+ 		res.render('index',{
+ 			title:'测试 首页',
+ 			movies:movies
+ 		})
  	})
  })
 
-  app.get('/movie/:id',function(req, res){
- 	res.render('detail',{
- 		title:'测试 详情页'
- 	})
+app.get('/movie/:id',function(req, res){
+  	var id=req.params.id
+
+  	Movie.findById(id,function(err,movie){
+  		res.render('detail',{
+ 			title:'测试'+ movie.title,
+ 			movie:movie
+ 		})
+  	})
  })
 
-   app.get('/admin/movie',function(req, res){
+app.get('/admin/movie',function(req, res){
  	res.render('admin',{
- 		title:'测试 录入页'
+ 		title:'测试 录入页',
+ 		movie:[{
+ 			title:'',
+ 			director:'',
+ 			country:'',
+ 			year:'',
+ 			poster:'',
+ 			flash:'',
+ 			summary:'',
+ 			lanugage:''
+ 		}]
  	})
  })
 
-    app.get('/admin/list',function(req, res){
- 	res.render('list',{
- 		title:'测试 列表'
+app.get('/admin/update/:id',function(req,res){
+	var id=req.params.id
+	if(id){
+		Movie.findById(id,function(){
+			res.render('admin',{
+				title:'后台更新页',
+				movie:movie
+			})
+		})
+	}
+})
+
+app.post('/admin/movie/new',function(res,req){
+	var id=req.body.movie.__dirname
+	var movieObj=req.body.movie
+	var _movie
+	if(id!=="undefinded"){
+		Movie.findById(id,function(err,movie){
+			if (err) {
+				console.log(err)
+			}
+			_movie=_.extend(movie,movieObj)
+			_movie.save(function(err,movie){
+				if(err){
+					console.log(err)
+				}
+				red.redirect('/movie/'+movie._id)
+			})
+		})
+	}else{
+		_movie=new Movie({
+			title:movieObj.title,
+			director:movieObj.director,
+ 			country:movieObj.country,
+ 			year:movieObj.year,
+ 			poster:movieObj.poster,
+ 			flash:movieObj.flash,
+ 			summary:movieObj.summary,
+ 			lanugage:movieObj.lanugage
+		})
+		_movie.save(function(err,movie){
+			if(err){
+				console.log(err)
+			}
+			red.redirect('/movie/'+movie._id)
+		})
+	}
+
+})
+
+app.get('/admin/list',function(req, res){
+	Movie.fetch(function(err, movies){
+ 		if (err) {
+ 			console.log(err)
+ 		}
+ 		res.render('list',{
+ 			title:'测试 列表',
+ 			movies:movies
+ 		})
  	})
  })
