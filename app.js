1240a59f982afc844125a459f2cb54d4a6ddb754
林建歆 <qq@qq.com>
2016-03-08 16:27:06 +0800

    文件修改

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views','./views/pages')
 app.set('view engine','jade')
 /*app.use(bodyParser.json())*/
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname,'bower_components')))
+app.use(express.static(path.join(__dirname,'public')))
 app.locals.moment=require('moment')
 app.listen(port)
 
@@ -51,16 +51,16 @@ app.get('/movie/:id',function(req, res){
 //admin page
 app.get('/admin/movie',function(req, res){
  	res.render('admin',{
- 		title:'测试 录入页',
+ 		title:'录入页',
  		movie:[{
- 			title:'',
- 			director:'',
- 			country:'',
- 			lanugage:'',
- 			year:'',
- 			poster:'',
- 			flash:'',
- 			summary:''
+ 			title:' ',
+ 			director:' ',
+ 			country:' ',
+ 			lanugage:' ',
+ 			year:' ',
+ 			poster:' ',
+ 			flash:' ',
+ 			summary:' '
  		}]
  	})
  })
@@ -69,16 +69,20 @@ app.get('/admin/movie',function(req, res){
 app.get('/admin/update/:id',function(req,res){
 	var id=req.params.id
 	if(id){
-		Movie.findById(id,function(){
-			res.render('admin',{
-				title:'后台更新页',
-				movie:movie
-			})
+		Movie.findById(id,function(err,movie){
+			if(err){
+				console.log(err)
+			}else{
+				res.render('admin',{
+					title:'后台更新页',
+					movie:movie
+				})
+			}
 		})
 	}
 })
 
-app.post('/admin/movie/new',function(res,req){
+app.post('/admin/movie/new',function(req,res){
 	var id=req.body.movie._id
 	var movieObj=req.body.movie
 	var _movie
@@ -127,3 +131,17 @@ app.get('/admin/list',function(req, res){
  		})
  	})
  })
+
+//delete
+app.delete('/admin/list',function(req,res){
+	var id=req.query.id
+	if(id){
+		Movie.remove({_id:id},function(err,movie){
+			if(err){
+				console.log(err)
+			}else{
+				res.json({success:1})
+			}
+		})
+	}
+})
